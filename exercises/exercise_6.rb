84b0d0844106f3ac9799fc2fc8a1da90f64c0179
Hosu Lee <sogeumhosu@gmail.com>
2023-11-19 17:14:57 -0800

    exercise 6 completed

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
+@store1.employees.create(first_name: "Harry", last_name: "Potter", hourly_rate: 45)
+
+@store1.employees.create(first_name: "Lord", last_name: "Voldemort", hourly_rate: 55)
+
+@store2.employees.create(first_name: "Ron", last_name: "Weasley", hourly_rate: 45)
+@store2.employees.create(first_name: "Hermione", last_name: "Granger", hourly_rate: 60)
+@store2.employees.create(first_name: "Serverus", last_name: "Snape", hourly_rate: 50)
