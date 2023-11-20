47bd51e1e5213d0657b4ad8f81696f42b15e5368
Hosu Lee <sogeumhosu@gmail.com>
2023-11-19 17:42:01 -0800

    exercise 7 completed

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,30 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Please enter a store name:"
+store_name = gets.chomp
+
+puts "Enter annual revenue:"
+annual_revenue = gets.chomp.to_i # convert string to integer
+
+puts "Does the store carry men's apparel? (yes/no)"
+mens_apparel = gets.chomp.downcase == 'yes'
+
+puts "Does the store carry women's apparel? (yes/no)"
+womens_apparel = gets.chomp.downcase == 'yes' 
+
+new_store = Store.new(
+  name: store_name, 
+  annual_revenue: annual_revenue, 
+  mens_apparel: mens_apparel, 
+  womens_apparel: womens_apparel
+)
+
+if new_store.save
+  puts "Store created successfully!"
+else
+  puts "Error: Unable to create store."
+  new_store.errors.full_messages.each do |message|
+    puts message
+  end
+end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,16 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  validate :must_carry_at_least_one_apparel
+
+  private
+
+  def must_carry_at_least_one_apparel
+    if mens_apparel.blank? && womens_apparel.blank?
+      errors.add(:base, "Stores must carry at least one of the men's or women's apparel")
+    end
+  end
 end
